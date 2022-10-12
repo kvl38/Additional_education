800fa26fe98df948e38cb91836f6943d5ed6d6dc
kvl <pat38ru@gmail.com>
2022-10-12 10:20:46 +0300

    Added task #2

--- Task_number_1.py
+++ Task_number_1.py
@@ -7,46 +7,65 @@ def Calculator(x, y, sign):
     elif sign == '*': result *= y
     elif sign == '/':
         if y == 0:
-            # print('Error, Деление на 0!')
             return "Error, Деление на 0!"
         else: result /= y
     elif sign == '^': result **= y
-    elif sign == '%': result %= y
+    elif sign == '%':
+        if y == 0:
+            return "Error, Деление на 0!"
+        else: result %= y
     elif sign == 's': result **= (1/y)
     else: print('Error')
     return result
 
-stop = ''
-res = None
-while stop != '=':
-    if res != None: print(f"\nПромежуточный результат = {res}")
-    print('\n-----Калькулятор-----\n'
-          '\'+\' - сложение\n'
-          '\'-\' - вычитание\n'
-          '\'*\' - умножение\n'
-          '\'/\' - деление\n'
-          '\'^\' - возведение в степень\n'
-          '\'%\' - остаток от деления\n'
-          '\'s\' - извлечение корня\n'
-          'Введите действие: ', end=" ")
-    if res == None:
+def Ent_values(result):
+    if result == None:
         sign = input()
         print('Введите первое число', end=": ")
         x = int(input())
-        print('Введите второе число', end=": ")
+        if sign == '^':
+            print('Введите степень числа', end=": ")
+        elif sign == 's':
+            print('Введите степень корня числа', end=": ")
+        else:
+            print('Введите второе число', end=": ")
         y = int(input())
-        res = Calculator(x, y, sign)
+        result = Calculator(x, y, sign)
     else:
         sign = input()
-        x = res
-        print('Введите второе число', end=": ")
+        x = result
+        if sign == '^':
+            print('Введите степень числа', end=": ")
+        elif sign == 's':
+            print('Введите степень корня числа', end=": ")
+        else:
+            print('Введите второе число', end=": ")
         y = int(input())
-        res = Calculator(x, y, sign)
+        result = Calculator(x, y, sign)
+    return result
 
-    print(f'\nРезультат = {res}'
-          '\nЕсли вы завершили расчеты, введите "="\n'
-          'Для продолжения подсчета нажмите Enter\n'
-          'Ваше действие: ', end=" ")
-    stop = input()
-    print("----------------------------------")
-print(f"Конечный результат: {res}")
+
+if __name__ == '__main__':
+    stop = ''
+    res = None
+    while stop != '=':
+        if res != None: print(f"\nПромежуточный результат = {res}")
+        print('\n-----Калькулятор-----\n'
+              '\'+\' - сложение\n'
+              '\'-\' - вычитание\n'
+              '\'*\' - умножение\n'
+              '\'/\' - деление\n'
+              '\'^\' - возведение в степень\n'
+              '\'%\' - остаток от деления\n'
+              '\'s\' - извлечение корня\n'
+              'Введите действие: ', end=" ")
+
+        res = Ent_values(res)
+
+        print(f'\nРезультат = {res}'
+              '\nЕсли вы завершили расчеты, введите "="\n'
+              'Для продолжения подсчета нажмите Enter\n'
+              'Ваше действие: ', end=" ")
+        stop = input()
+        print("----------------------------------")
+    print(f"Конечный результат: {res}")
